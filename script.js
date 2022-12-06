24dec6fe5ff47a6688174852a12a3b6107ec584a
tobuya <thomasobuya95@yahoo.com>
2022-12-06 22:40:20 +0300

    Add movie class

---
+++ script.js
@@ -0,0 +1,18 @@
+// Movie Class: Represents a movie
+class Movie {
+    constructor (title, producer, year) {
+        this.title = title;
+        this.producer = producer;
+        this.year = year;
+    }
+}
+
+//UI Class: Handle UI Tasks
+
+//Store Class: Handle Local Storage
+
+//Event: Display Movies
+
+//Event: Add a Movie
+
+//Event: Remove a Movie
