94ea6539657f5a7b00fac145ace83dfeb47342f3
tobuya <thomasobuya95@yahoo.com>
2022-12-09 08:32:01 +0300

    Add User Interface class

--- script.js
+++ script.js
@@ -8,6 +8,42 @@ class Movie {
 }
 
 //UI Class: Handle UI Tasks
+class UI {
+    static displayMovies() {
+        const storedMovies = [
+            {
+                title: "Black Panther",
+                producer: "Marvel",
+                year: 2017
+            },
+            {
+                title: "Banshee",
+                producer: "Marvel",
+                year: 2002
+            }
+        ]
+
+        const movies = storedMovies;
+
+        movies.forEach((movie) => UI.addMovieToList(movie));
+    }
+
+    static addMovieToList(movie) {
+        const list = document.getElementById('book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+          <td>${movie.title}</td>
+          <td>${movie.producer}</td>
+          <td>${movie.year}</td>
+          <td>
+            <a href="#" class="btn btn-danger btn-sm delete">remove</a>
+          </td>
+        `
+        list.appendChild(row);
+    }
+}
 
 //Store Class: Handle Local Storage
 
